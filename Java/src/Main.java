86fdfb088a561d6c17e0a6f7f5d5ddfd595714ec
Samuel M. Weiss <samuelweiss@earthlink.net>
2013-02-23 16:43:47 -0600

    Moving in JME3 files and working on main

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,6 +1,25 @@
 import cs1.*;
-public class Main {
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.*;
+import com.jme3.math.ColorRGBA;
+
+
+public class Main extends SimpleApplication {
 	public static void main (String[]args){
+		Main app = new Main();
+		app.start();
 		Keyboard.readInt();
+	}
+	@Override
+	public void simpleInitApp(){
+		Box b = new Box(Vector3f.ZERO, 1, 1, 1);
+		Geometry geom = new Geometry("box", b);
+		Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+		mat.setColor("Color", ColorRGBA.Blue);
+		geom.setMaterial(mat);
+		rootNode.attachChild(geom);
 	}
 }
